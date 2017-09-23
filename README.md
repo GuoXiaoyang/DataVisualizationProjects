dc307e423b24c138c422487e36fc2bcfc447ea70
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-09-23 18:07:04 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Game of Life
+
+A project for [FreeCodeCampus challenges](https://www.freecodecamp.org/challenges/visualize-data-with-a-bar-chart).
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+[Demo](https://guoxiaoyang.github.io/game-of-life/)
+
---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/App.css';
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() {
+   return (
+      <div className="App">
+s
+      </div>    
+    );
+  }
+}
+
+
+
+export default App;
